c7b9914d396a2927f3edc059a4dec774915a8c2d
Alex <alexander.semenov@ecoation.com>
2022-04-11 13:45:50 -0400

    writing files working

---
+++ kortex_scripts/src/positions_to_file.py
@@ -0,0 +1,64 @@
+#!/usr/bin/env python3
+import sys
+import copy
+from turtle import position
+import rospy
+import moveit_commander
+import moveit_msgs.msg
+import geometry_msgs.msg
+import json
+import signal
+
+joint_state_topic = ['joint_states:=/my_gen3/joint_states']
+moveit_commander.roscpp_initialize(joint_state_topic)
+rospy.init_node('get_moveit_data', anonymous=True)
+
+robot = moveit_commander.RobotCommander(robot_description="/my_gen3/robot_description") 
+group = moveit_commander.MoveGroupCommander(robot_description="my_gen3/robot_description", ns="/my_gen3", name="arm")
+
+def signal_handler(sig, frame):
+    print('You pressed Ctrl+C!')
+    f.close()
+    sys.exit(0)
+
+signal.signal(signal.SIGINT, signal_handler)
+
+name = input("enter name of file, ctrl c to quit \n")
+
+try:
+    f = open(name, mode='a', encoding='utf-8')
+    # with open("test.txt", mode='a', encoding='utf-8') as file:
+    #     print("opend another?")
+
+except IOError:
+    print("Error")
+
+while True:
+    
+    input("press enter to save position, ctrl c to quit \n")
+    pos = group.get_current_pose().pose
+    pos_dict = {"position":{ "x": pos.position.x,
+                            "y": pos.position.y,
+                            "z": pos.position.z
+                },
+                "orientation":{ "x": pos.orientation.x,
+                                "y": pos.orientation.y,
+                                "z": pos.orientation.z,
+                                "w": pos.orientation.w
+                },
+    }
+
+    f.write(json.dumps(pos_dict) + "\n")
+    print("wrote: " + json.dumps(pos_dict) + "\n")
+        
+
+# print ("Reference frame: %s" % group.get_planning_frame())
+# print ("End effector: %s" % group.get_end_effector_link())
+# print ("Current Joint Values:")
+# print (group.get_current_joint_values())
+# print ("Robot Groups:")
+# print (robot.get_group_names())
+# print ("Current Pose:")
+# print (group.get_current_pose())
+# print ("Robot State:")
+# print (robot.get_current_state())
