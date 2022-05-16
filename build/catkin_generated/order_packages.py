84accdce29dcdcee30a5f506f7e4e1d7a16b6f0d
Alex <alexander.semenov@ecoation.com>
2022-05-16 14:52:01 -0400

    added mag gripper sim functionality

--- build/catkin_generated/order_packages.py
+++
@@ -1,5 +0,0 @@
-# generated from catkin/cmake/template/order_packages.context.py.in
-source_root_dir = '/pkg/src'
-whitelisted_packages = ''.split(';') if '' != '' else []
-blacklisted_packages = ''.split(';') if '' != '' else []
-underlay_workspaces = '/opt/ros/noetic'.split(';') if '/opt/ros/noetic' != '' else []
